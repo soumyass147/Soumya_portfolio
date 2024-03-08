10f381cd5c2a2e7d4f4e896af78ae6e6f205b1a1
soumyass147 <soumyastudy147@gmail.com>
2024-03-08 19:30:31 +0530

    added new sections

--- script.js
+++ script.js
@@ -54,3 +54,18 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+///------tablinks----/////
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
