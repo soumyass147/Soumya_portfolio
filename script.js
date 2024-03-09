755343eb2037e3023922818c51da65089e02b371
soumyass147 <soumyastudy147@gmail.com>
2024-03-09 17:22:50 +0530

    added footer and fixed form padding

--- script.js
+++ script.js
@@ -69,3 +69,43 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+///=========google sheet linkage to form----------//////
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwBDmieI9OJWRQLMZLWwl9WIQMSt-gNgtoqs52hlM436__hu3mJjTfv8vqZ7GzpAzbs/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
+
+
+/////--------======tab links ====--------////
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200";
+    
+}
