1b79c41a6052aea6a89494e8b01577019a6d0c52
MAVERICK-VF142 <ashutosh.jena.mav.1@gmail.com>
2024-03-10 00:36:04 +0530

    fixed the sidebar issue

--- script.js
+++ script.js
@@ -69,3 +69,27 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right="0";
+}
+
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+
+//----------google sheet--------
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwBDmieI9OJWRQLMZLWwl9WIQMSt-gNgtoqs52hlM436__hu3mJjTfv8vqZ7GzpAzbs/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
+
